86d78182554c9e0c1b34707391989d8dfc63e9ed
Zach Holman <zach@zachholman.com>
2013-06-19 18:00:26 -0700

    ⒷⓊⒷⓈ

---
+++ lib/bubs.rb
@@ -0,0 +1,71 @@
+# encoding: utf-8
+class Bubs
+  VERSION = '0.0.1'
+
+  # Convert words to ⓌⓄⓇⒹⓈ.
+  #
+  # Returns a String, but a much cooler string than what you had initially.
+  def self.convert(text)
+    bubs = {
+      :A => :Ⓐ,
+      :B => :Ⓑ,
+      :C => :Ⓒ,
+      :D => :Ⓓ,
+      :E => :Ⓔ,
+      :F => :Ⓕ,
+      :G => :Ⓖ,
+      :H => :Ⓗ,
+      :I => :Ⓘ,
+      :J => :Ⓙ,
+      :K => :Ⓚ,
+      :L => :Ⓛ,
+      :M => :Ⓜ,
+      :N => :Ⓝ,
+      :O => :Ⓞ,
+      :P => :Ⓟ,
+      :Q => :Ⓠ,
+      :R => :Ⓡ,
+      :S => :Ⓢ,
+      :T => :Ⓣ,
+      :U => :Ⓤ,
+      :V => :Ⓥ,
+      :W => :Ⓦ,
+      :X => :Ⓧ,
+      :Y => :Ⓨ,
+      :Z => :Ⓩ,
+      :a => :ⓐ,
+      :b => :ⓑ,
+      :c => :ⓒ,
+      :d => :ⓓ,
+      :e => :ⓔ,
+      :f => :ⓕ,
+      :g => :ⓖ,
+      :h => :ⓗ,
+      :i => :ⓘ,
+      :j => :ⓙ,
+      :k => :ⓚ,
+      :l => :ⓛ,
+      :m => :ⓜ,
+      :n => :ⓝ,
+      :o => :ⓞ,
+      :p => :ⓟ,
+      :q => :ⓠ,
+      :r => :ⓡ,
+      :s => :ⓢ,
+      :t => :ⓣ,
+      :u => :ⓤ,
+      :v => :ⓥ,
+      :w => :ⓦ,
+      :x => :ⓧ,
+      :y => :ⓨ,
+      :z => :ⓩ
+    }
+
+    bubbled = text.split(//).map do |letter|
+        bubs[letter.to_sym] || letter
+    end.join('')
+
+    `echo "#{bubbled}" | pbcopy` if RUBY_PLATFORM =~ /darwin/
+    puts bubbled
+  end
+end
